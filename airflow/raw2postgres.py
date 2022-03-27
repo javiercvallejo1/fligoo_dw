9926bd3bfd726458ede1b252489a036d1e3039ed
javiercvallejo1 <javiercvallejo1@hotmail.com>
2022-03-27 16:07:39 -0500

    tweaking values

--- airflow/raw2postgres.py
+++ airflow/raw2postgres.py
@@ -121,7 +121,7 @@ default_args = {
 #tasks
 dag = DAG('fligoo_dw_write',
           default_args=default_args,
-          schedule_interval='0 8 * * *',
+          schedule_interval='0 8 * * 1',
           catchup=False
           )
 
@@ -135,20 +135,20 @@ end = DummyOperator(
     dag=dag)
 
 send_request=PythonOperator(task_id='send_request'
-            ,provide_context=True
+            ,provide_context=False
             ,python_callable=extract_data
             ,dag=dag
             )
 
 crush_data=PythonOperator(task_id='crush_data'
-            ,provide_context=True
+            ,provide_context=False
             ,python_callable=process_data
             ,dag=dag
             )
 
 
 write_data=PythonOperator(task_id='write_data'
-            ,provide_context=True
+            ,provide_context=False
             ,python_callable=write_db
             ,dag=dag
             )
